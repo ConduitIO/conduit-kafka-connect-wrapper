96a2d26809b2176b199bb3ab6550bd8649661ce0
Haris Osmanagić <haris@meroxa.io>
2022-02-19 13:01:04 +0100

    load libs from dir

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,1 +1,5 @@
-cp kafka-connector-plugin ../conduit/pkg/plugins/
+rm -rf ../conduit/pkg/plugins/kafka-connector-plugin/
+mkdir -p ../conduit/pkg/plugins/kafka-connector-plugin/
+
+cp kafka-connector-plugin ../conduit/pkg/plugins/kafka-connector-plugin/
+cp -r libs/ ../conduit/pkg/plugins/kafka-connector-plugin/
--- scripts/package.sh
+++ scripts/package.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 mvn clean package
-echo '#!/usr/bin/env java -jar' > kafka-connector-plugin
-cat target/kafka-connector-plugin-*.jar >> kafka-connector-plugin
+echo '#!/usr/bin/env java -cp "kafka-connector-plugin-0.1.0-SNAPSHOT.jar:libs/*" io.conduit.Application' > kafka-connector-plugin
 chmod +x kafka-connector-plugin
