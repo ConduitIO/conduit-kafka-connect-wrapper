39e1eeae212150bf72329b3b875a6d0e9668bf79
Haris Osmanagić <haris@meroxa.io>
2022-02-19 14:37:31 +0100

    different script

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,5 +1,6 @@
 rm -rf ../conduit/pkg/plugins/kafka-connector-plugin/
 mkdir -p ../conduit/pkg/plugins/kafka-connector-plugin/
 
-cp kafka-connector-plugin ../conduit/pkg/plugins/kafka-connector-plugin/
-cp -r libs/ ../conduit/pkg/plugins/kafka-connector-plugin/
+cp scripts/kafka-connector-plugin ../conduit/pkg/plugins/kafka-connector-plugin/
+cp -r libs/ ../conduit/pkg/plugins/kafka-connector-plugin/libs/
+cp target/kafka-connector-plugin-0.1.0-SNAPSHOT.jar ../conduit/pkg/plugins/kafka-connector-plugin/
--- scripts/package.sh
+++ scripts/package.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
 mvn clean package
-echo '#!/usr/bin/env java -cp "kafka-connector-plugin-0.1.0-SNAPSHOT.jar:libs/*" io.conduit.Application' > kafka-connector-plugin
-chmod +x kafka-connector-plugin
