042d14fcfd4fb10e9f681eb271f0775fec158f1e
Haris Osmanagić <haris@meroxa.io>
2022-02-22 13:16:48 +0100

    changes

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,6 +1,7 @@
-rm -rf ../conduit/pkg/plugins/kafka-connector-plugin/
-mkdir -p ../conduit/pkg/plugins/kafka-connector-plugin/
+TO_DIR=../conduit/pkg/plugins/kafka-connector-plugin/
+rm -rf $TO_DIR
+mkdir -p $TO_DIR
 
-cp scripts/kafka-connector-plugin ../conduit/pkg/plugins/kafka-connector-plugin/
-cp -r libs/ ../conduit/pkg/plugins/kafka-connector-plugin/libs/
-cp target/kafka-connector-plugin-0.1.0-SNAPSHOT.jar ../conduit/pkg/plugins/kafka-connector-plugin/
+cp scripts/kafka-connector-plugin $TO_DIR
+cp -r libs/ $TO_DIR/libs/
+cp target/kafka-connector-plugin-0.1.0-SNAPSHOT.jar $TO_DIR
