4618e99f91ad365c99ef7cf243603e36f62d4308
Haris Osmanagić <haris@meroxa.io>
2022-03-09 11:42:34 +0100

    little bit more

--- src/main/java/io/conduit/DefaultSchemaProvider.java
+++ src/main/java/io/conduit/DefaultSchemaProvider.java
@@ -1,5 +1,9 @@
 package io.conduit;
 
+import java.util.Map;
+
+import com.google.protobuf.Struct;
+import com.google.protobuf.Value;
 import io.conduit.grpc.Record;
 import org.apache.kafka.connect.data.Schema;
 
@@ -12,6 +16,18 @@ public class DefaultSchemaProvider implements SchemaProvider {
 
     @Override
     public Schema provide(Record record) {
+        if (!record.hasPayload()) {
+            return null;
+        }
+        if (record.getPayload().hasStructuredData()) {
+            return provideForStructured(record);
+        }
+        return null;
+    }
+
+    private Schema provideForStructured(Record record) {
+        Struct struct = record.getPayload().getStructuredData();
+        Map<String, Value> fieldsMap = struct.getFieldsMap();
         return null;
     }
 }
