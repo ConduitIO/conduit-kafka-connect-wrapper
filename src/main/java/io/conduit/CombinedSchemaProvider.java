0a3830ee1ef3bca64dc4b3576c59e6219a9f00a3
Haris Osmanagić <haris@meroxa.io>
2022-03-09 14:24:28 +0100

    checkstyle

--- src/main/java/io/conduit/CombinedSchemaProvider.java
+++ src/main/java/io/conduit/CombinedSchemaProvider.java
@@ -1,14 +1,34 @@
+/*
+ * Copyright 2022 Meroxa, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.conduit;
 
 import io.conduit.grpc.Record;
 import org.apache.kafka.connect.data.Schema;
 
+/**
+ * A {@link SchemaProvider} implementation which combines the functionality of
+ * a {@link RawDataSchemaProvider} and a {@link StructSchemaProvider}.
+ */
 public class CombinedSchemaProvider implements SchemaProvider {
-    private final RawDataSchemaProvider jsonSP;
+    private final RawDataSchemaProvider rawDataSP;
     private final StructSchemaProvider structSP;
 
     public CombinedSchemaProvider(String name, Schema overrides) {
-        this.jsonSP = new RawDataSchemaProvider(name, overrides);
+        this.rawDataSP = new RawDataSchemaProvider(name, overrides);
         this.structSP = new StructSchemaProvider(name, overrides);
     }
 
@@ -19,6 +39,9 @@ public class CombinedSchemaProvider implements SchemaProvider {
         }
         if (record.getPayload().hasStructuredData()) {
             return structSP.provide(record);
+        }
+        if (record.getPayload().hasRawData()) {
+            return rawDataSP.provide(record);
         }
         return null;
     }
