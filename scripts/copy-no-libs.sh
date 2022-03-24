53b6d05487a86830ff1a11c59ed273b1ae42755f
Haris Osmanagić <haris@meroxa.io>
2022-03-24 12:13:39 +0100

    Rename project to  conduit-kafka-connect-wrapper (#42)

--- scripts/copy-no-libs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Copies the script and the connector JAR into dist/.
-TO_DIR=dist/
-cp scripts/kafka-connector-plugin $TO_DIR
-cp target/kafka-connector-plugin-0.1.0-SNAPSHOT.jar $TO_DIR
--- scripts/copy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Copies the script, the connector JAR and pre-packaged dependencies into dist/.
-TO_DIR=dist/
-mkdir -p $TO_DIR
-
-cp scripts/kafka-connector-plugin $TO_DIR
-cp -r libs/ $TO_DIR/libs/
-cp target/kafka-connector-plugin-0.1.0-SNAPSHOT.jar $TO_DIR
