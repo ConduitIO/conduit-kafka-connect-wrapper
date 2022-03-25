1a741af435a731f67ff049b5f96dc4e49375a8ce
Haris Osmanagić <haris@meroxa.io>
2022-03-25 10:31:02 +0100

    Use connector class instead of task class (#45)

--- src/test/java/io/conduit/ConfigTest.java
+++ src/test/java/io/conduit/ConfigTest.java
@@ -10,14 +10,14 @@ public class ConfigTest {
     @Test
     public void testFromMap() {
         Map<String, String> map = Map.of(
-                "wrapper.task.class", "test-task-class",
+                "wrapper.connector.class", "test-connector-class",
                 "wrapper.connector.name", "test-connector-name",
                 "wrapper.pipeline.id", "test-pipeline-id",
                 "kafka.connector.param1", "test-value-1",
                 "kafka.connector.param2", "test-value-2"
         );
         Config cfg = Config.fromMap(map);
-        assertEquals(map.get("wrapper.task.class"), cfg.getTaskClass());
+        assertEquals(map.get("wrapper.connector.class"), cfg.getConnectorClass());
         assertEquals(map.get("wrapper.connector.name"), cfg.getConnectorName());
         assertEquals(map.get("wrapper.pipeline.id"), cfg.getPipelineId());
         assertEquals(
