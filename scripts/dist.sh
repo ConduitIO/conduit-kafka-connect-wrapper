18276d0c3848a35ffe9bdf6512ca36a0e0817add
Haris Osmanagic <haris@meroxa.io>
2022-04-05 14:16:53 +0200

    Increment snapshot version

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -4,4 +4,4 @@ mvn clean package
 TO_DIR=dist/
 mkdir -p $TO_DIR/libs
 cp scripts/conduit-kafka-connect-wrapper $TO_DIR
-cp target/conduit-kafka-connect-wrapper-0.1.0-SNAPSHOT.jar $TO_DIR
+cp target/conduit-kafka-connect-wrapper-*.jar $TO_DIR
