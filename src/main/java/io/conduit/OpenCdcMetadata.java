72846357b51165ee5a8602c006c420b1f8ca7f0c
Haris Osmanagic <haris@meroxa.io>
2022-08-25 18:48:19 +0200

    checkstyle

--- src/main/java/io/conduit/OpenCdcMetadata.java
+++ src/main/java/io/conduit/OpenCdcMetadata.java
@@ -23,6 +23,7 @@ public final class OpenCdcMetadata {
     private OpenCdcMetadata() {
 
     }
+
     /**
      * CreatedAt can contain the time when the record was created in the 3rd party system.
      * The expected format is a unix timestamp in nanoseconds.
