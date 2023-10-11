95634d2e234af5873466a192ed87845ff8f4025a
Haris Osmanagic <haris@meroxa.io>
2023-10-11 16:38:09 +0200

    Expose Kafka connector configs

--- src/main/java/io/conduit/SpecifierService.java
+++ src/main/java/io/conduit/SpecifierService.java
@@ -16,9 +16,12 @@
 
 package io.conduit;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 import io.conduit.grpc.Specifier;
 import io.conduit.grpc.Specifier.Parameter.Validation;
@@ -30,6 +33,8 @@ import io.github.classgraph.ClassInfo;
 import io.github.classgraph.ClassInfoList;
 import io.github.classgraph.ScanResult;
 import io.grpc.stub.StreamObserver;
+import lombok.SneakyThrows;
+import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.connect.connector.Connector;
 import org.apache.kafka.connect.sink.SinkConnector;
 import org.apache.kafka.connect.source.SourceConnector;
@@ -64,69 +69,61 @@ public class SpecifierService extends SpecifierPluginGrpc.SpecifierPluginImplBas
         responseObserver.onCompleted();
     }
 
-    private Map<String, Specifier.Parameter> buildDestinationParams() {
-        return Map.of(
-            "wrapper.connector.class",
-            Specifier.Parameter.newBuilder()
-                .setDescription(
-                    "The class of the requested connector. "
-                        + "It needs to be found on the classpath, i.e. in a JAR in the `libs` directory. ")
-                .setDefault("none")
-                .addValidations(requiredValidation)
-                .addValidations(availableConnectorsValidation(SinkConnector.class))
-                .build(),
-
-            "wrapper.schema",
-            Specifier.Parameter.newBuilder()
-                .setDescription("The schema of the records which will be written to a destination connector.")
-                .setDefault("none")
-                .build(),
-
-            "wrapper.schema.autogenerate.enabled",
-            Specifier.Parameter.newBuilder()
-                .setDescription(
-                    "Automatically generate schemas (destination connector). "
-                        + "Cannot be `true` if a schema is set.")
-                .setDefault("none")
-                .build(),
-
-            "wrapper.schema.autogenerate.name",
-            Specifier.Parameter.newBuilder()
-                .setDescription("Name of automatically generated schema. "
-                    + "Required if schema auto-generation is turned on.")
-                .setDefault("none")
-                .build(),
-
-            "wrapper.schema.autogenerate.overrides",
-            Specifier.Parameter.newBuilder()
-                .setDescription("A (partial) schema which overrides types in the auto-generated schema.")
-                .setDefault("none")
-                .build()
-        );
+    private Map<String, Specifier.Parameter> buildSourceParams() {
+        Map<String, Specifier.Parameter> params = new HashMap<>();
+        params.put("wrapper.connector.class", Specifier.Parameter.newBuilder()
+            .setDescription(
+                "The class of the requested connector. "
+                    + "It needs to be found on the classpath, i.e. in a JAR in the `libs` directory. ")
+            .setDefault("none")
+            .addValidations(requiredValidation)
+            .addValidations(availableConnectorsValidation(SourceConnector.class))
+            .build());
+        params.putAll(underlyingSourceParams());
+        return params;
     }
 
-    private Map<String, Specifier.Parameter> buildSourceParams() {
-        return Map.of(
-            "wrapper.connector.class",
-            Specifier.Parameter.newBuilder()
-                .setDescription(
-                    "The class of the requested connector. "
-                        + "It needs to be found on the classpath, i.e. in a JAR in the `libs` directory. ")
-                .setDefault("none")
-                .addValidations(requiredValidation)
-                .addValidations(availableConnectorsValidation(SourceConnector.class))
-                .build()
-        );
+    private Map<String, Specifier.Parameter> buildDestinationParams() {
+        Map<String, Specifier.Parameter> params = new HashMap<>();
+        params.put("wrapper.connector.class", Specifier.Parameter.newBuilder()
+            .setDescription(
+                "The class of the requested connector. "
+                    + "It needs to be found on the classpath, i.e. in a JAR in the `libs` directory. ")
+            .setDefault("none")
+            .addValidations(requiredValidation)
+            .addValidations(availableConnectorsValidation(SinkConnector.class))
+            .build());
+        params.put("wrapper.schema", Specifier.Parameter.newBuilder()
+            .setDescription("The schema of the records which will be written to a destination connector.")
+            .setDefault("none")
+            .build());
+        params.put("wrapper.schema.autogenerate.enabled", Specifier.Parameter.newBuilder()
+            .setDescription(
+                "Automatically generate schemas (destination connector). "
+                    + "Cannot be `true` if a schema is set.")
+            .setDefault("none")
+            .build());
+        params.put("wrapper.schema.autogenerate.name", Specifier.Parameter.newBuilder()
+            .setDescription("Name of automatically generated schema. "
+                + "Required if schema auto-generation is turned on.")
+            .setDefault("none")
+            .build());
+        params.put("wrapper.schema.autogenerate.overrides", Specifier.Parameter.newBuilder()
+            .setDescription("A (partial) schema which overrides types in the auto-generated schema.")
+            .setDefault("none")
+            .build());
+        params.putAll(underlyingSinkParams());
+        return params;
     }
 
     private Validation availableConnectorsValidation(Class<? extends Connector> connectorClass) {
         return Validation.newBuilder()
             .setType(Validation.Type.TYPE_INCLUSION)
-            .setValue(loadAvailableConnectors(connectorClass))
+            .setValue(String.join(",", loadAvailableConnectors(connectorClass)))
             .build();
     }
 
-    private String loadAvailableConnectors(Class<? extends Connector> connectorClass) {
+    private List<String> loadAvailableConnectors(Class<? extends Connector> connectorClass) {
         List<String> connectors = new LinkedList<>();
 
         ClassGraph cg = new ClassGraph()
@@ -144,6 +141,74 @@ public class SpecifierService extends SpecifierPluginGrpc.SpecifierPluginImplBas
         }
 
         logger.info("Found connectors {}", connectors);
-        return String.join(",", connectors);
+        return connectors;
+    }
+
+    @SneakyThrows
+    private Map<String, Specifier.Parameter> underlyingSinkParams() {
+        Map<String, Specifier.Parameter> params = new HashMap<>();
+        for (String clazz : loadAvailableConnectors(SinkConnector.class)) {
+            SinkConnector connector = (SinkConnector) newInstance(clazz);
+            connector.config()
+                .configKeys()
+                .forEach((keyName, key) -> params.put(clazz + "." + keyName, toConduitParam(key)));
+        }
+
+        return params;
+    }
+
+    @SneakyThrows
+    private Map<String, Specifier.Parameter> underlyingSourceParams() {
+        Map<String, Specifier.Parameter> params = new HashMap<>();
+        for (String clazz : loadAvailableConnectors(SourceConnector.class)) {
+            SourceConnector connector = (SourceConnector) newInstance(clazz);
+            connector.config()
+                .configKeys()
+                .forEach((keyName, key) -> params.put(clazz + "." + keyName, toConduitParam(key)));
+        }
+
+        return params;
+    }
+
+    private Specifier.Parameter toConduitParam(ConfigDef.ConfigKey key) {
+        return Specifier.Parameter.newBuilder()
+            .setDefault(String.valueOf(key.defaultValue))
+            .setDescription(key.documentation)
+            .setType(toConduitType(key.type))
+            // todo
+            .addAllValidations(List.of())
+            .build();
+    }
+
+    private Specifier.Parameter.Type toConduitType(ConfigDef.Type type) {
+        switch (type) {
+            case BOOLEAN -> {
+                return Specifier.Parameter.Type.TYPE_BOOL;
+            }
+            case STRING, PASSWORD, CLASS, LIST -> {
+                return Specifier.Parameter.Type.TYPE_STRING;
+            }
+            case INT, LONG, SHORT -> {
+                return Specifier.Parameter.Type.TYPE_INT;
+            }
+            case DOUBLE -> {
+                return Specifier.Parameter.Type.TYPE_FLOAT;
+            }
+            default -> throw new IllegalStateException("Unexpected value: " + type);
+        }
+    }
+
+    // copied from TaskFactory
+    @SneakyThrows
+    private Object newInstance(String className) {
+        Class<?> clazz = Class.forName(className);
+        return Arrays.stream(clazz.getConstructors())
+            .filter(c -> c.getParameterCount() == 0)
+            .findFirst()
+            // get() also throws NoSuchElementException, but here:
+            // 1. we send the caller more details
+            // 2. we also adhere to a best practice (not calling get() without checking if value is present)
+            .orElseThrow(() -> new NoSuchElementException("no parameterless constructor for " + className))
+            .newInstance();
     }
 }
--- src/test/java/io/conduit/SpecifierServiceTest.java
+++ src/test/java/io/conduit/SpecifierServiceTest.java
@@ -18,12 +18,17 @@ package io.conduit;
 
 import java.util.Arrays;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 import io.conduit.grpc.Specifier;
 import io.conduit.grpc.Specifier.Parameter.Validation;
 import io.conduit.grpc.Specifier.Specify.Request;
 import io.conduit.grpc.Specifier.Specify.Response;
 import io.grpc.stub.StreamObserver;
+import lombok.SneakyThrows;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.sink.SinkConnector;
+import org.apache.kafka.connect.source.SourceConnector;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 
@@ -68,26 +73,80 @@ class SpecifierServiceTest {
 
         assertNotNull(response.getSourceParamsMap());
         assertFalse(response.getSourceParamsMap().isEmpty());
-        verifyWrapperClassValidations(
+        verifyWrapperClass(
             new String[]{
                 "io.aiven.connect.jdbc.JdbcSourceConnector",
                 "io.debezium.connector.postgresql.PostgresConnector"
             },
             response.getSourceParamsMap()
         );
+        verifySourceParams(
+            "io.aiven.connect.jdbc.JdbcSourceConnector",
+            response.getSourceParamsMap()
+        );
+        verifySourceParams(
+            "io.debezium.connector.postgresql.PostgresConnector",
+            response.getSourceParamsMap()
+        );
 
         assertNotNull(response.getDestinationParamsMap());
         assertFalse(response.getDestinationParamsMap().isEmpty());
-        verifyWrapperClassValidations(
+        verifyWrapperClass(
             new String[]{
-                "com.snowflake.kafka.connector.SnowflakeSinkConnector",
-                "io.aiven.connect.jdbc.JdbcSinkConnector"
+                "io.aiven.connect.jdbc.JdbcSinkConnector",
+                "com.snowflake.kafka.connector.SnowflakeSinkConnector"
             },
+            response.getDestinationParamsMap()
+        );
+        verifyDestinationParams(
+            "io.aiven.connect.jdbc.JdbcSinkConnector",
+            response.getDestinationParamsMap()
+        );
+        verifyDestinationParams(
+            "com.snowflake.kafka.connector.SnowflakeSinkConnector",
             response.getDestinationParamsMap()
         );
     }
 
-    private void verifyWrapperClassValidations(String[] expected, Map<String, Specifier.Parameter> params) {
+    @SneakyThrows
+    private void verifySourceParams(String clazz, Map<String, Specifier.Parameter> paramsMap) {
+        SourceConnector connector = (SourceConnector) newInstance(clazz);
+        connector.config()
+            .configKeys()
+            .forEach((keyName, key) -> verifyParam(clazz, key, paramsMap));
+    }
+
+    @SneakyThrows
+    private void verifyDestinationParams(String clazz, Map<String, Specifier.Parameter> paramsMap) {
+        SinkConnector connector = (SinkConnector) newInstance(clazz);
+        connector.config()
+            .configKeys()
+            .forEach((keyName, key) -> verifyParam(clazz, key, paramsMap));
+    }
+
+    private void verifyParam(String clazz, ConfigDef.ConfigKey key, Map<String, Specifier.Parameter> paramsMap) {
+        assertTrue(
+            paramsMap.containsKey(clazz + "." + key.name),
+            "expected params to contain: " + key.name
+        );
+        Specifier.Parameter param = paramsMap.get(clazz + "." + key.name);
+        assertEquals(key.documentation, param.getDescription());
+    }
+
+    @SneakyThrows
+    private Object newInstance(String className) {
+        Class<?> clazz = Class.forName(className);
+        return Arrays.stream(clazz.getConstructors())
+            .filter(c -> c.getParameterCount() == 0)
+            .findFirst()
+            // get() also throws NoSuchElementException, but here:
+            // 1. we send the caller more details
+            // 2. we also adhere to a best practice (not calling get() without checking if value is present)
+            .orElseThrow(() -> new NoSuchElementException("no parameterless constructor for " + className))
+            .newInstance();
+    }
+
+    private void verifyWrapperClass(String[] expected, Map<String, Specifier.Parameter> params) {
         var validations = params.get("wrapper.connector.class").getValidationsList();
         assertNotNull(validations);
         assertEquals(2, validations.size());
@@ -101,9 +160,9 @@ class SpecifierServiceTest {
             .filter(v -> v.getType() == Validation.Type.TYPE_INCLUSION)
             .findFirst();
         assertTrue(values.isPresent());
-        Arrays.sort(expected);
         var actual = values.get().getValue().split(",");
         Arrays.sort(actual);
+        Arrays.sort(expected);
         assertArrayEquals(expected, actual);
     }
 }
